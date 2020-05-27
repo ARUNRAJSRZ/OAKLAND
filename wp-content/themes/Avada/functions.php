efcfab3b4e1bdfc1d85e079ea4bef074bbbf502e
arunraj <srzarunraj@gmail.com>
2020-05-27 16:30:51 +0530

    first commit

---
+++ wp-content/themes/Avada/functions.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Extra files & functions are hooked here.
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package Avada
+ * @subpackage Core
+ * @since 1.0
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+
+if ( ! defined( 'AVADA_VERSION' ) ) {
+	define( 'AVADA_VERSION', '6.2.2' );
+}
+
+if ( ! defined( 'AVADA_MIN_PHP_VER_REQUIRED' ) ) {
+	define( 'AVADA_MIN_PHP_VER_REQUIRED', '5.6' );
+}
+
+if ( ! defined( 'AVADA_MIN_WP_VER_REQUIRED' ) ) {
+	define( 'AVADA_MIN_WP_VER_REQUIRED', '4.7' );
+}
+
+// Developer mode.
+if ( ! defined( 'AVADA_DEV_MODE' ) ) {
+	define( 'AVADA_DEV_MODE', false );
+}
+
+/**
+ * Compatibility check.
+ *
+ * Check that the site meets the minimum requirements for the theme before proceeding.
+ *
+ * @since 6.0
+ */
+if ( version_compare( $GLOBALS['wp_version'], AVADA_MIN_WP_VER_REQUIRED, '<' ) || version_compare( PHP_VERSION, AVADA_MIN_PHP_VER_REQUIRED, '<' ) ) {
+	require_once get_template_directory() . '/includes/bootstrap-compat.php';
+	return;
+}
+
+/**
+ * Bootstrap the theme.
+ *
+ * @since 6.0
+ */
+require_once get_template_directory() . '/includes/bootstrap.php';
+
+/* Omit closing PHP tag to avoid "Headers already sent" issues. */
+
+add_action( 'load-index.php', 'bbloomer_direct_access_wp_dashboard_redirect' );
+ 
+function bbloomer_direct_access_wp_dashboard_redirect(){
+  wp_redirect( admin_url( 'admin.php?page=wc-admin' ) );
+}
+ 
+add_filter( 'login_redirect', 'bbloomer_login_wp_dashboard_redirect', 9999, 3 );
+ 
+function bbloomer_login_wp_dashboard_redirect( $redirect_to, $request, $user ){
+  $redirect_to = admin_url( 'admin.php?page=wc-admin' );
+  return $redirect_to;
+}
+
+function filter_rest_allow_anonymous_comments() {
+return true;
+}
+add_filter('rest_allow_anonymous_comments','filter_rest_allow_anonymous_comments');
